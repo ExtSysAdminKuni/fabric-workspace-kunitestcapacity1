eebfb3fe067edbbd6fab0a4c878ec28e27928ed1
ExtSysAdminKuni <admin@MngEnvMCAP709727.onmicrosoft.com>
2026-08-30 13:43:11 +0900

    Replace v_test view with v_banner_location_repro view

--- kunitestcapacity1/Gold_Repro.Warehouse/silver/Views/v_test.sql
+++ kunitestcapacity1/Gold_Repro.Warehouse/silver/Views/v_test.sql
@@ -1,4 +1,18 @@
 -- Auto Generated (Do not modify) CD2FCB62F84DA2915124E33E40EC7A11C3E86CA35D6D83FD34874E4E574AAEF2
-CREATE VIEW silver.v_test
+CREATE VIEW [silver].[v_banner_location_repro]
 AS
-SELECT 1 AS col1;
+WITH SourceData AS
+(
+    SELECT *
+    FROM[v_banner_location_reproData AS
+ *
+anner_location] AS whbl
+    LEFT JOIN [Silver_Repro].[data_warehouse].[banner_location_group] AS whblg
+        ON whbl.banner_location_id = whblg.banner_location_id
+)
+SELECT
+    banner_location_id,
+    silver_created_date,
+    source_modified_date
+FROM SourceData;
+GO
