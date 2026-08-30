c22fd5deb4e5d617eaab980cb334f8d4e1eea105
ExtSysAdminKuni <admin@MngEnvMCAP709727.onmicrosoft.com>
2026-08-30 14:35:57 +0900

    Fix formatting in v_banner_location_repro.sql

--- kunitestcapacity1/Gold_Repro.Warehouse/silver/Views/v_banner_location_repro.sql
+++ kunitestcapacity1/Gold_Repro.Warehouse/silver/Views/v_banner_location_repro.sql
@@ -4,9 +4,9 @@ AS
 WITH SourceData AS
 (
     SELECT 
-      whbl.banner_location_id,
-      whbl.silver_created_date,
-      whbl.source_modified_date
+      banner_location_id,
+      silver_created_date,
+      source_modified_date
     FROM [Silver_Repro].[data_warehouse].[banner_location] AS whbl
     LEFT JOIN [Silver_Repro].[data_warehouse].[banner_location_group] AS whblg
         ON whbl.banner_location_id = whblg.banner_location_id
