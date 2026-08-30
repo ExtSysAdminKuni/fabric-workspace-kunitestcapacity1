c4d27c8e1856d5140c2841ef0e07b49840dac5fb
ExtSysAdminKuni <admin@MngEnvMCAP709727.onmicrosoft.com>
2026-08-30 13:53:07 +0900

    Refactor v_banner_location_repro view definition

--- kunitestcapacity1/Gold_Repro.Warehouse/silver/Views/v_test.sql
+++ kunitestcapacity1/Gold_Repro.Warehouse/silver/Views/v_test.sql
@@ -3,7 +3,10 @@ CREATE VIEW [silver].[v_banner_location_repro]
 AS
 WITH SourceData AS
 (
-    SELECT *
+    SELECT
+        whbl.banner_location_id,
+        whbl.silver_created_date,
+        whbl.source_modified_date
     FROM [Silver_Repro].[data_warehouse].[banner_location] AS whbl
     LEFT JOIN [Silver_Repro].[data_warehouse].[banner_location_group] AS whblg
         ON whbl.banner_location_id = whblg.banner_location_id
